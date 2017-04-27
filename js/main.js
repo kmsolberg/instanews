31aee1eae724bacd958afe7f7d159363e8c17b7c
Kat Solberg <katsolberg@gmail.com>
2017-04-27 15:25:00 -0700

    images loaded from API

--- js/main.js
+++ js/main.js
@@ -1,22 +1,29 @@
 $(function () {
-  
+  var storyGrid = "";
+  var picture = "";
   $('#section').change(function (event) {
     event.preventDefault();
     $('header').css('height', 'auto');
     $('.loading').before('<img src="assets/images/ajax-loader.gif" id="loading-gif">');
     var selection = $('#section option').filter(':selected').val();
-    var url = "https://api.nytimes.com/svc/topstories/v2/" + selection + ".json"
-    console.log(url);
-        url += '?' + $.param({'api-key': "c15852cbf6e143a28c25363796863ee0"
+    var urlSource = "https://api.nytimes.com/svc/topstories/v2/" + selection + ".json"
+        urlSource += '?' + $.param({'api-key': "c15852cbf6e143a28c25363796863ee0"
         });
     $.ajax({
-    url: url,
+    url: urlSource,
     method: 'GET',
-    }).done(function(result) {
-    console.log(result);
-    }).fail(function(err) {
-    throw err;
+  }).done(function(data) {
+    $.each(data.results, function ( key, value ) {
+      if (value.multimedia[3]) {
+        picture = value.multimedia[3].url
+        storyGrid += '<li><img src="' + picture + '">' + value.abstract + '</li>'  
+      }
     });
+    console.log(storyGrid);
+    $(".stories").append(storyGrid);
+  }).fail(function(err) {
+    throw err;
+    });  
   });
 });
 
